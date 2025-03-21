fbf5b4e8d8f78776937b28bc5f03a88c769618eb
Derrick <similienderrick@yahoo.com>
2025-03-21 01:41:07 -0400

    Initialized git repository

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import axios from 'axios'
+
+const Posts = () => {
+  const { id } = useParams();
+  const [posts, setPosts] = useState([]);
+   
+useEffect(() => {
+  async function fetchPosts() {
+  const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+  setPosts(data);
+  } 
+  fetchPosts(); 
+
+}, []);
+  return (
+    <div>
+      {posts.map(post => <div>{post.title}</div>)}
+    </div>
+  );
+}
+
+export default Posts;
